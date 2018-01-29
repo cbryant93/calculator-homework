e01cec4f24d9a1f79bec190a338d37900d77e2c0
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-01-29 21:03:36 +0000

    bmi calculator complete

--- app.js
+++ app.js
@@ -1,39 +1,47 @@
+var calculator = prompt("Which calculator will you like?: (a)Basic (b)Advanced (c)BMI");
+switch(calculator){
+  case 'a': var x = parseInt(prompt('Enter a Value: ', '0'));
+            var y = parseInt(prompt('Enter a Value: ', '0'));
 
-var calculator = prompt("Which calculator will you like?: (a)Basic (b)Advanced");
-if(calculator === 'a'){
-  var x = parseInt(prompt('Enter a Value: ', '0'));
-  var y = parseInt(prompt('Enter a Value: ', '0'));
-
-  var num1 = parseInt(x);
-  var num2 = parseInt(y);
+            var num1 = parseInt(x);
+            var num2 = parseInt(y);
 
 
-  var calculator = prompt("What would will you like to do with the numbers?: (+)Add (-)Subtract (*)multiplication (/)division");
+            var calculator = prompt("What would will you like to do with the numbers?: (+)Add (-)Subtract (*)multiplication (/)division");
 
-  switch(calculator){
-  case '+': alert(num1 + num2);
-  break;
-  case '-': alert(num1 - num2);
-  break;
-  case '*': alert(num1 * num2);
-  break;
-  case '/': alert(num1 / num2);
-  break;
-  default: alert("What Happend");
-  }
+            switch(calculator){
+            case '+': alert(num1 + num2);
+            break;
+            case '-': alert(num1 - num2);
+            break;
+            case '*': alert(num1 * num2);
+            break;
+            case '/': alert(num1 / num2);
+            break;
+            default: alert("What Happend?");
+            }
+    break;
 
-}else{
-    var calculator = prompt("What would will you like to do with the numbers?: (pow)Power (rt)Square Root");
+ case 'b':     var calculator = prompt("What would will you like to do with the numbers?:  (pow)Power (rt)Square Root");
 
-  switch(calculator){
-  case 'rt': var x = parseInt(prompt('Enter a Value: ', '0'));
-              var num1 = Math.sqrt(x);
-  break;
-  case 'pow': var y = parseInt(prompt('Enter a Value: ', '0'));
-              var z = parseInt(prompt('Enter a Value: ', '0'));
-              var power = Math.pow(y,z);
-              alert(power);
-  break;
-  default: alert("What Happend");
-  }
-}
+           switch(calculator){
+           case 'rt': var x = parseInt(prompt('Enter a Value: ', '0'));
+                       var num1 = Math.sqrt(x);
+           break;
+           case 'pow': var y = parseInt(prompt('Enter a Value: ', '0'));
+                       var z = parseInt(prompt('Enter a Value: ', '0'));
+                       var power = Math.pow(y,z);
+                       alert(power);
+           break;
+           default: alert("What Happend?");
+           }
+    break;
+
+ case 'c': var kg = parseInt(prompt('Enter your weight in Kg: ', '0'));
+
+           var height = parseFloat(prompt('Enter height in metres: ','0'));
+           var heightSquared = Math.pow(height,2);
+           alert(kg / heightSquared);
+ break;
+ default: alert("What Happend?");
+            }
